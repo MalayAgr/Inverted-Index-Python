0cda0977304cec0e42940896fbb98ec7de4dcdde
Malay Agarwal <malay.agarwal261016@outlook.com>
2017-12-23 13:06:06 +0530

    Add TODO

--- Inverted Index.py
+++ Inverted Index.py
@@ -1,5 +1,10 @@
 import os
 
+#TODO - Optimizations
+#TODO - decrease for loops used somehow
+#TODO - try applying map and reduce
+#TODO - make directory user-selected
+
 
 def createDictionary():
 
